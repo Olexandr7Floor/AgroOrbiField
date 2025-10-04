83b36b023f3ee27959215f84cccdb630199ca2e9
NEMO1337 <101059091+NEMO246@users.noreply.github.com>
2025-10-04 16:11:07 +0300

    scenario main

---
+++ scripts/scenarios.js
@@ -0,0 +1,12 @@
+const SCENARIO_MANIFEST = {
+    easy: [
+        "easy_scenario_1.js",
+        "easy_scenario_2.js"
+    ],
+    medium: [
+        "medium_scenario_1.js"
+    ],
+    hard: [
+        "hard_scenario_1.js"
+    ]
+};
